ad7268b0e5b3507664ae989846b842dce33aa4ea
Niklas Neesemann <n.eesemann@gmail.com>
2022-07-25 16:34:33 +0200

    refactored base layout. added homecontroller

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,27 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+// import './scss/styles.scss';
 
 var $ = require("jquery");
 // start the Stimulus application
 import './bootstrap';
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+});
+
